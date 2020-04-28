5ad93022df40571e73fcd7f4493ee91646dd94b4
Saravana <saravana.27@hotmail.com>
2020-04-28 10:45:59 +0530

    project initiated

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/anu126/user-details
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.2 // indirect
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/anu126/user-details/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1")
+	{
+		user := new(controllers.HelloWorldController())
+		v1.GET("/user", user)
+	}
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"message": "Not found"})
+	})
+
+	router.Run(":5000")
+}
